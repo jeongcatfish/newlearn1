03dd3129ff02f2a9146ef977daf27864d0d3a895
JM <39644789+jeongcatfish@users.noreply.github.com>
2021-12-02 18:23:12 +0900

    회원가입 인증메일 작업 중

--- src/main/java/com/learn/newlearn1/account/AccountController.java
+++ src/main/java/com/learn/newlearn1/account/AccountController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.time.LocalDateTime;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,6 +24,7 @@ public class AccountController {
 
     private final SignUpFormValidator signUpFormValidator;
     private final AccountService accountService;
+    private final AccountRepository accountRepository;
 
     //PostMapping(/sign-up) 에서 signUpForm을 받을 때 실행 됨.
     @InitBinder("signUpForm")
@@ -46,6 +48,30 @@ public class AccountController {
         accountService.processNewAccount(signUpForm);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/check-email-token")
+    public String checkEmailToken(String token, String email, Model model){
+
+        Account account = accountRepository.findByEmail(email);
+        String view = "account/checked-email";
+
+        if(account == null){
+            model.addAttribute("error","wrong_email");
+            return view;
+        }
+
+        if(!account.getEmailCheckToken().equals(token)){
+            model.addAttribute("error","w");
+            return view;
+        }
+
+        account.setEmailVerified(true);
+        account.setJoinedAt(LocalDateTime.now());
+        model.addAttribute("numberOfuser", accountRepository.count());
+        model.addAttribute("nickname", account.getNickname());
+
+        return view;
     }
 
     @GetMapping("/fag")
